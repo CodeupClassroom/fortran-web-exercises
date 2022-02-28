e2ed3af4517159d990fb13c65a89bf9501ae9e8a
Ry Sutton <ryancesutton@gmail.com>
2022-02-28 09:33:05 -0600

    completed removeAll warmup

--- js/warmups.js
+++ js/warmups.js
@@ -13,3 +13,26 @@ function findAverage(grades) {
 }
 
 console.log(findAverage([95, 74, 86, 100]));
+
+
+function removeAll(anArr, valueToRemove) {
+    let filteredArr = [];
+    // anArr.forEach(function (ele) {
+    //     if (valueToRemove !== ele) {
+    //         filteredArr.push(ele);
+    //     }
+    // });
+
+    for (let i = 0; i < anArr.length; i++) {
+        if (valueToRemove === anArr[i]) {
+            continue;
+        }
+        filteredArr.push(anArr[i]);
+    }
+
+    return filteredArr;
+}
+
+let bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+
+console.log(removeAll(bugs, "ant"));
