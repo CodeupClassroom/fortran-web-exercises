5d52a66df31d42d4daa0a60a61257fc101bda5e3
Ry Sutton <ryancesutton@gmail.com>
2022-03-07 12:24:47 -0600

    completed dom events lec

--- js/dom-events-lec.js
+++ js/dom-events-lec.js
@@ -1,3 +1,57 @@
 "use strict";
 
 
+let handler = function (event) {
+    alert("You clicked something!");
+}
+
+// FUNCTION EXPRESSION ONLY ACCESIBLE BELOW
+// document.addEventListener("click", handler);
+
+
+// THREE STEPS OF EVENT LISTENERS
+// ACCESS (GRAB) ELEMENT
+let header = document.querySelector("#main-header");
+
+// ATTACH EVENT LISTENER
+header.addEventListener("click", headerClick);
+
+// WHAT HAPPENS ON HEADER CLICK
+function headerClick(event) {
+    header.style.backgroundColor = "yellow";
+}
+
+
+let input = document.querySelector("#search");
+
+// input.addEventListener("keyup", function (event) {
+//     // PRINT ELEMENT
+//     console.log(event.target);
+//
+//     // PRINT KEY CODE
+//     console.log(event.code);
+// });
+
+input.addEventListener("keyup", upperCaseTextInput)
+
+function upperCaseTextInput(event) {
+    console.log(event.target.value);
+    event.target.value = event.target.value.toUpperCase();
+}
+
+
+let moreBtn = document.querySelector("#more");
+
+moreBtn.addEventListener("click", function (e) {
+    let p = e.target.previousElementSibling
+    if (p.classList.contains("hidden")) {
+        p.classList.remove("hidden");
+        e.target.innerText = "Less";
+    } else {
+        p.classList.add("hidden");
+        e.target.innerText = "More";
+    }
+});
+
+
+
